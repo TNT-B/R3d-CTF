7a09b4b0b27b1032324f6865ea70404aae458ef4
TNT-B <thanhbinh8223@gmail.com>
2025-07-20 09:16:21 +0700

    Test document fix git

--- CTFd/testdocument.py
+++ CTFd/testdocument.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, render_template
 
-from CTFd.utils import config
-from CTFd.utils.config.visibility import scores_visible
-from CTFd.utils.decorators.visibility import (
-    check_account_visibility,
-    check_score_visibility,
-)
+# from CTFd.utils import config
+# from CTFd.utils.config.visibility import scores_visible
+# from CTFd.utils.decorators.visibility import (
+#     check_account_visibility,
+#     check_score_visibility,
+# )
 # from CTFd.utils.decorators import authed_only
 # from CTFd.utils.helpers import get_infos
 # from CTFd.utils.scores import get_standings
